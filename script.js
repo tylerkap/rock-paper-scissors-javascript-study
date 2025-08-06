5cd01f162398258e35648221ad97747970068981
Tyler Kap <tylerkap55@gmail.com>
2025-08-06 10:05:52 -0600

    Implemented Human Choice Function

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const prompt=require("prompt-sync")({sigint:true});
+
+
 function getComputerChoice() {
 
     let randomNum = Math.floor(Math.random() * 3);
@@ -12,4 +15,31 @@ function getComputerChoice() {
     }
 }
 
+function getHumanChoice () {
+    
+    let isValidChoice = false;
+
+    while (isValidChoice === false) {
+        let humanChoice = prompt("Let's Play a Game! Enter - 0: rock,  1: paper, 2: scissors: ");
+        let humanChoiceNum = Number(humanChoice);
+
+        if (humanChoice >= 0 && humanChoice <= 2) {
+            switch (humanChoiceNum) {
+                case 0:
+                    return "rock";
+                case 1:
+                    return "paper"
+                case 2:
+                    return "scissors"
+            }
+        }
+        else {
+            console.log("Invalid response. Try again.");
+            isValidChoice = false;
+        }
+    }
+
+}
+
 console.log(getComputerChoice());
+console.log(getHumanChoice());
